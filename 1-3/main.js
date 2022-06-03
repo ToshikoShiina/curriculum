f92b031fac285caa915618f57aea12e35a55dad1
ToshikoShiina <73923959+ToshikoShiina@users.noreply.github.com>
2022-06-03 18:37:45 +0900

    1-3課題提出
    1-3JavaScriptの課題の提出です。
    よろしくお願いします。

---
+++ 1-3/main.js
@@ -0,0 +1,29 @@
+// 問１-1
+let a = 15;
+if (a != 10){
+    console.log('10ではありません');
+}
+
+// 問1-2
+let b = 12;
+if(b >= 10 && b < 20){
+    console.log(b);
+}if(b % 2 === 0 ){
+    console.log(b);
+}
+
+// 問2
+let score = 10;
+if(score >= 10 && score <=  20){
+    console.log('成功です');
+} else {
+    console.log('失敗です');
+}
+
+// 問3
+let y = 0;
+if (y % 2 === 0){
+    console.log('偶数です');
+} if (b % 2 !== 0) {
+    console.log('奇数です');    
+} 
